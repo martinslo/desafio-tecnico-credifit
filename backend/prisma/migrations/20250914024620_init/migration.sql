185984f5153407e7ee127ab7c19278e17fc7f6aa
Lucas de Oliveira <lucasmo@outlook.com.br>
2025-09-13 23:46:43 -0300

    feat: add user in prisma

---
+++ backend/prisma/migrations/20250914024620_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "public"."Usuario" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usuario_email_key" ON "public"."Usuario"("email");
