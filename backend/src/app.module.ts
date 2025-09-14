650a4469e5aee99e0537e880717112cfd89010e4
Lucas de Oliveira <lucasmo@outlook.com.br>
2025-09-14 15:39:16 -0300

    feat: add imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from 'prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { EmprestimosModule } from './emprestimos/emprestimos.module';
+import { EmpresasModule } from './empresas/empresas.module';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    UsuarioModule,
+    EmprestimosModule,
+    EmpresasModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
